a2f354a575a3312996559195e535b2d887c11b0c
franky4p <39118102+franky4p@users.noreply.github.com>
2019-09-10 14:44:24 +0300

    Update settings.py

--- DjangoMagazine/DjangoMagazine/settings.py
+++ DjangoMagazine/DjangoMagazine/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'DjangoMagazine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['app/templates/app'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
